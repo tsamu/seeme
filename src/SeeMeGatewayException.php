5aa7274170c6ec12c14add3dfce4e5a9533c4d04
tsamu <tamas.samu@gmail.com>
2017-01-31 16:16:29 +0100

    init package

---
+++ src/SeeMeGatewayException.php
@@ -0,0 +1,12 @@
+<?php
+namespace SeeMe;
+class SeeMeGatewayException extends Exception {
+    public function __construct($response,$errorCode) {
+        $this->response		= $response;
+        $this->errorCode    = $errorCode;
+        parent::__construct( $response, $errorCode );
+    }
+    public function __toString() {
+        return __CLASS__ . ": SEEME_GATEWAY_ERROR #{$this->errorCode}, {$this->response}\n";
+    }
+}
